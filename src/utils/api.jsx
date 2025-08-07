e11a0fd426ba249126478a54336f6d9c114a07f4
jithin <jithinjzx@gmail.com>
2025-08-07 15:31:17 +0530

    api

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: ' https://souled-db-3.onrender.com'
+  baseURL: 'https://souled-db-3.onrender.com'
 });
 
 export default api;
 
 // http://localhost:3000
+// https://souled-db-3.onrender.com
